6924aad377170ff5bb859f5a02b470b767780b60
Einsgates <jh79@illinois.edu>
2021-05-16 22:20:45 +0800

    costFun

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -20,10 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = 1 ./ (1 + e .^ (-X * theta));
 
+J = -1/m * sum(y' * log(h) + (1 - y)' * log(1 - h));
 
-
-
+grad = 1/m * (X' * (h - y));
 
 
 
