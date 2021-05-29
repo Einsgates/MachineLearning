7f410ba59ff8d22d110d7cfe3df05aa0fb336865
Einsgates <jh79@illinois.edu>
2021-05-29 13:48:31 +0800

    CostFun

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -37,8 +37,13 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
+J = 1/m * sum(-y' * log(h) - (1 - y)' * log(1 - h)) + lambda / (2*m) * sum(theta(2:(size(theta,1))).^2);
 
+grad = 1/m * (X' * (h - y)) + lambda/m*theta;
+grad_tmp = 1/m * (X' * (h - y));
 
+grad(1) = grad_tmp(1);
 
 
 
