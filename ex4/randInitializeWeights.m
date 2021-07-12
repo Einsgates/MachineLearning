b85492ebf8cc880671e49327e06420d7a8df233b
Einsgates <jh79@illinois.edu>
2021-07-12 15:45:44 +0800

    randInitWgts

--- ex4/randInitializeWeights.m
+++ ex4/randInitializeWeights.m
@@ -19,9 +19,9 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
+INIT_EPSILON = sqrt(6) / sqrt(L_in + L_out);
 
-
-
+W = 2 * INIT_EPSILON * rand(L_out, L_in + 1) - INIT_EPSILON;
 
 
 
